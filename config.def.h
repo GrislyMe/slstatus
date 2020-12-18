d30f2e2a90e432c4cd6407a5bc37b28960f6ea6d
GrislyMe <GrislyMe@csie.io>
2020-12-18 18:06:19 +0800

    show battery_perc and charging icon while charging

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* interval between updates (in ms) */
-const unsigned int interval = 1000;
+const unsigned int interval = 10000;
 
 /* text to show if no value can be retrieved */
 static const char unknown_str[] = "n/a";
@@ -63,5 +63,9 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+	{datetime, "| %s - ", "%h-%d"},
+	{datetime, "%s | ", "%H:%M"},
+	{run_command, "%s ", "python ~/src/slstatus/scripts/batteryIcon.py"},
+    {battery_remaining, "%s | ", "BAT0"},
+    {wifi_perc, "  %s", "wlp3s0"}
 };
--- scripts/batteryIcon.py
+++ scripts/batteryIcon.py
@@ -1,12 +1,19 @@
 with open("/sys/class/power_supply/BAT0/capacity") as bat:
     perc = int(bat.readline())
     if perc > 90:
-        print("")
+        print("", end = "")
     elif perc > 65:
-        print("")
+        print("", end = "")
     elif perc > 45:
-        print("")
+        print("", end = "")
     elif perc > 25:
-        print("")
+        print("", end = "")
     else:
-        print("")
+        print("", end = "")
+
+with open("/sys/class/power_supply/BAT0/status") as status:
+    current = status.readline().split()
+    if current[0] != "Discharging":
+        print("  ﮣ", perc)
+    else:
+        print(" ")
